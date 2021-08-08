0edc456096dec91780caac09e5010c6ae19ff33b
Tevintaylor <tevintaylor_1996@yahoo.com>
2021-08-09 05:46:00 +0800

    Displaying number of results found from a search

--- classes/SiteResultsProvider.php
+++ classes/SiteResultsProvider.php
@@ -8,12 +8,11 @@ class SiteResultsProvider{
 
     public function getNumResults($term){
         $query = $this->con->prepare("SELECT COUNT(*) as total
-                                      FROM sites WHERE title 
-                                      LIKE :term 
+                                      FROM sites WHERE title LIKE :term 
                                       OR url LIKE :term
                                       OR keywords LIKE :term
                                       OR description LIKE :term");
-        $searchTerm = "%".$term."%";                             
+        $searchTerm = "%" . $term . "%";                             
         $query -> bindParam(':term',$searchTerm);
         $query -> execute();
 
--- search.php
+++ search.php
@@ -60,10 +60,11 @@ include("classes/SiteResultsProvider.php");
             </ul>
         </div>
     </div>
-    <div class="mainResultSection">
-        <?php echo
+    <div class="mainResultsSection">
+        <?php
             $resultsProvider = new SiteResultsProvider($con);
-            echo $resultsProvider->getNumResults($term);
+            $numResults = $resultsProvider->getNumResults($term);
+            echo "<p class='resultsCount'>$numResults results found</p>";
         ?>
     </div>
 
