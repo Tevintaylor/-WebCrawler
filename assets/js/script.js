948f1d6429e50cc7536c95e8c63a9e6d97fb5a64
Tevintaylor <tevintaylor_1996@yahoo.com>
2021-08-10 08:59:36 +0800

    update pagination and styling and added javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
     $(".result").on("click", function() {
         console.log("i was clicked");
+
+        var url = $(this).attr("href");
+        console.log(url)
+
+        var id = $(this).attr("data-linkId");
+        console.log(id)
+
+
+        if (!id) {
+            alert("data-linkId attribute not specified")
+        }
+
+        increaseLinkClicks(id, url);
         //return false; PREVENTS THE LINK FROM GOING TO A PAGE WHEN CLICKED
         return false;
     });
 });
+
+function increaseLinkClicks(linkId, url) {
+
+}
